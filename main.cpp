8c5726dc9128995a8409bb86ede707dd81947fd1
José Rodolfo <68907543+joserodolfo16@users.noreply.github.com>
2020-08-03 18:58:58 -0300

    Primeiro repositório git

--- main.cpp
+++ main.cpp
@@ -3,6 +3,24 @@ using namespace std;
 
 int main()
 {
-	cout << "Arquivo cpp é tóis :// ";
+	float a, b, rsoma, rsub, rdiv, rmul;
+	cout << "Calculadora simples em C++ " << endl;
+	cout << "Digite os dois números a serem calculados:" << endl;
+	cin >> a;
+	cin >> b;
+	rsoma = a + b;
+	rsub = a - b;
+	rmul = a*b;
+	if (b == 0)
+	{
+		cout << "Soma: " << rsoma << endl << "Subtração: " <<  rsub << endl <<
+		"Multiplicação: " << rmul << endl << "Divisão impossível."; 
+	}
+	else {
+		rdiv = a/b;
+		cout << "Soma: " << rsoma << endl << "Subtração: " << rsub << endl <<
+		"Multiplicação: " <<  rmul << endl << "Divisão :" << rdiv; 
+	}
+	
 	return 0;
 }
