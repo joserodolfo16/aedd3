fa64195e596095aba979ce0fa85d177de8b7c9ef
José Rodolfo <68907543+joserodolfo16@users.noreply.github.com>
2020-08-03 20:00:42 -0300

    Criação

--- main.cpp
+++ main.cpp
@@ -1,26 +1,21 @@
 #include <iostream>
 using namespace std;
-
-int main()
-{
-	float a, b, rsoma, rsub, rdiv, rmul;
-	cout << "Calculadora simples em C++ " << endl;
-	cout << "Digite os dois números a serem calculados:" << endl;
-	cin >> a;
-	cin >> b;
-	rsoma = a + b;
-	rsub = a - b;
-	rmul = a*b;
-	if (b == 0)
-	{
-		cout << "Soma: " << rsoma << endl << "Subtração: " <<  rsub << endl <<
-		"Multiplicação: " << rmul << endl << "Divisão impossível."; 
-	}
-	else {
-		rdiv = a/b;
-		cout << "Soma: " << rsoma << endl << "Subtração: " << rsub << endl <<
-		"Multiplicação: " <<  rmul << endl << "Divisão :" << rdiv; 
-	}
-	
-	return 0;
-}
+ class celula
+ {
+ 	private:
+ 		string nome;
+ 		celula * proxima;
+ 		
+   public:
+   	celula()
+   	{
+	   	proxima = NULL;
+	   }
+ };
+ 
+ int main()
+ {
+ 	celula temp;
+ 	temp.
+ 	return 0;
+ }
